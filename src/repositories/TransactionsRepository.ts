f6dcffcc427147e87e8d62708f6b327fe047d23c
Augusto Calixto <gutodenovaes@outlook.com>
2020-08-26 00:06:51 -0300

    Todos desafios concluidos com sucesso

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,33 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    this.transactions.forEach((transaction: Transaction) => {
+      if (transaction.type === `income`) {
+        balance.income += transaction.value;
+        balance.total += transaction.value;
+      } else {
+        balance.outcome += transaction.value;
+        balance.total -= transaction.value;
+      }
+    });
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Transaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: RequestDTO): Transaction {
+    if (type !== `income` && type !== `outcome`) {
+      throw Error(`Incompatible type`);
+    }
+    if (
+      this.transactionsRepository.getBalance().total - value < 0 &&
+      type === `outcome`
+    ) {
+      throw Error(`Not enougth balance`);
+    }
+    const transaction = this.transactionsRepository.create({
+      id: ``,
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
